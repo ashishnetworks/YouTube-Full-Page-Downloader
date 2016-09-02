f8b0cd9430be87981cd88be0911d793e8ec73325
Ashish Networks <info2and@gmail.com>
2016-09-02 14:50:15 +0530

    added setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='YFPD',
+      version='0.1',
+      description='This programs helps in donloading all the vidios of front page of the you tube page for specific keywords',
+      url='https://bitbucket.org/ashishnetworks/telibot',
+      author='Ashish Networks',
+      author_email='info2and@gmail.com',
+      license='MIT',
+      #packages=['YFPD'],
+      install_requires=['requests','beautifulsoup4','html5lib','lxml'],
+
+      zip_safe=False)
