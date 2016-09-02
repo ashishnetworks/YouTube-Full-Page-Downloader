e6701970c369abbe57bf47343d7761906faf05c6
Ashish Networks <ashishnetworks@users.noreply.github.com>
2016-09-02 18:50:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='YFPD',
+setup(name='YouTube-Full-Page-Downloader',
       version='0.1',
       description='This programs helps in donloading all the vidios of front page of the you tube page for specific keywords',
       url='https://github.com/ashishnetworks/YouTube-Full-Page-Downloade',
