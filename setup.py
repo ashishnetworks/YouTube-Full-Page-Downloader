c87867cd87d5d2e312bd3524f9bdb2f90a83fb27
Ashish Networks <ashishnetworks@users.noreply.github.com>
2016-09-02 18:48:42 +0530

    corrected setup url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='YFPD',
       version='0.1',
       description='This programs helps in donloading all the vidios of front page of the you tube page for specific keywords',
-      url='https://bitbucket.org/ashishnetworks/telibot',
+      url='https://github.com/ashishnetworks/YouTube-Full-Page-Downloade',
       author='Ashish Networks',
       author_email='info2and@gmail.com',
       license='MIT',
